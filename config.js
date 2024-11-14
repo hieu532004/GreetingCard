7684dfb310df8bc2f67e00ee69c5e4e58404d5f9
TrungHieu <hieu532004@users.noreply.github.com>
2024-11-14 23:08:29 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Thầy cô Softech';
+const giftUrl = 'https://www.facebook.com/softechaptech/posts/869316832046643?ref=embed_post';
+const eventName = 'Chúc Mừng 20-11';
+const titleCard = 'Gửi tặng các thầy cô Softech';
+const contentCard = 'Chúc thầy cô Softech ngày 20-11 thật ý nghĩa và tràn đầy niềm vui! Cảm ơn thầy cô đã luôn nhiệt huyết truyền đạt kiến thức, là nguồn cảm hứng cho học viên. Mong thầy cô luôn mạnh khỏe, hạnh phúc và thành công trong sự nghiệp giáo dục!';
+const giftImage = 'img/bao-ve-do-an-sem-1-batch-175-softech-aptech.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
